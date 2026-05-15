6ed6b6163d6d6f7e7d5cf0684758724bf1b5c722
raulhernandezcastro-maker <raul.hernandez.castro@gmail.com>
2026-05-15 06:33:28 -0400

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,12 +5,12 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js')
 
 firebase.initializeApp({
-  apiKey: "AIzaSyC1kUOV03HaTvhJ-tTUtJtY6BsCRXwSFA4",
-  authDomain: "sindicato-liberty.firebaseapp.com",
-  projectId: "sindicato-liberty",
-  storageBucket: "sindicato-liberty.firebasestorage.app",
-  messagingSenderId: "394236327667",
-  appId: "1:394236327667:web:da323695cea4dcfd99e986"
+  apiKey: "AIzaSyD6gE205ttKOxnoiFub7AoIteyO8-9ICcM",
+  authDomain: "sindicato-generica.firebaseapp.com",
+  projectId: "sindicato-generica",
+  storageBucket: "sindicato-generica.firebasestorage.app",
+  messagingSenderId: "553378585081",
+  appId: "1:553378585081:web:180f6fc747a2bea2c88528"
 })
 
 const messaging = firebase.messaging()
@@ -21,7 +21,7 @@ messaging.onBackgroundMessage((payload) => {
 
   const { title, body, icon } = payload.notification || {}
 
-  self.registration.showNotification(title || 'Sindicato Liberty', {
+  self.registration.showNotification(title || 'Mi Sindicato', {
     body: body || 'Tienes un nuevo aviso',
     icon: icon || '/logo.png',
     badge: '/logo.png',
